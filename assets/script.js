a240157d09c53bfe4de9200b671b8e0af80d8993
Angela angie <angelajk496@gmail.com>
2026-05-15 13:28:17 +0300

    added bactics

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,11 @@ fetch("https://jsonplaceholder.typicode.com/users")
     .then(data => {
         console.log(data);
     } ) ;
+    //fetching 
+    fetch("https://jsonplaceholder.typicode.com/users")
+     .then(response => response.json())
+     .then(data => {
+        data.forEach(user => {
+            document.getElementById("users").innerHTML +=`<p>${user.name}</p>`
+        });
+        } ) ;
