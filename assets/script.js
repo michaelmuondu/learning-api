555b964f3a251a10371923e4628c7b94c71a1110
Angela angie <angelajk496@gmail.com>
2026-05-15 14:34:57 +0300

    added city

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,46 @@ fetch("https://jsonplaceholder.typicode.com/users")
             document.getElementById("users").innerHTML +=`<p>${user.name}</p>`
         });
         } ) ;
+        //emails
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(user => {
+                document.getElementById("emails").innerHTML += `<p>${user.email}</p>`
+            });
+        });
+        // user adress
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(data => { 
+            data.forEach(user => {
+                document.getElementById("address").innerHTML += `<p>${user.address} </p>`
+            });
+        });
+        //street
+          fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(user => {
+      document.getElementById("street").innerHTML += 
+        `<p>${user.address.street}</p>`;
+    });
+  });
+  //suite
+    fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(user => {
+      document.getElementById("suite").innerHTML += 
+        `<p>${user. address.suite}</p>`;
+    });
+  });
+//city
+ fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(user => {
+      document.getElementById("city").innerHTML += 
+        `<p>${user. address.city}</p>`;
+    });
+  });
